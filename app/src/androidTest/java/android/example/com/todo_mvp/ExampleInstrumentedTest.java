c323890c11dbb84d9d05b3d9bd2e7598c610c0f9
江国贸 <353793312@qq.com>
2018-05-05 17:18:29 +0800

    添加子模块

--- app/src/androidTest/java/android/example/com/todo_mvp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/android/example/com/todo_mvp/ExampleInstrumentedTest.java
@@ -20,7 +20,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
+        
         assertEquals("android.example.com.todo_mvp", appContext.getPackageName());
     }
 }
--- app/src/test/java/android/example/com/todo_mvp/ExampleUnitTest.java
+++ app/src/test/java/android/example/com/todo_mvp/ExampleUnitTest.java
@@ -1,17 +1,51 @@
 package android.example.com.todo_mvp;
 
+import org.hamcrest.Description;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.regex.Matcher;
 
 import static org.junit.Assert.*;
+import static org.junit.matchers.JUnitMatchers.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(Parameterized.class)
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    private String content;
+
+    public ExampleUnitTest(String content) {
+        this.content = content;
     }
+
+    @Parameterized.Parameters
+    public static Collection getString(){
+        return Arrays.asList(new String[]{
+                "Hello junit",
+                "Hello world",
+                "Fucker",
+        });
+
+    }
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        //assertEquals("鸡巴",4, 1 + 2);
+        //assertThat("鸡巴",50,);
+        assertEquals("鸡巴", content,"Hello junit");
+    }
+
+    @Test
+    public void test_1(){
+        assertEquals("鸡巴",5,1+3);
+    }
+
 }
